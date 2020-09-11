3a0b8207a47d79c38602ac26bf0ececdd594a210
brammetjedv <bramdevries2003@gmail.com>
2020-09-11 14:46:28 +0200

    opdracht

---
+++ Variabelen.py
@@ -0,0 +1,29 @@
+# String:
+naam = "Bram"
+# Boolean:
+aanUit = True
+# Float of double:
+puntGetal = 5.5
+# Character:
+karakter = 'o'
+# Integer:
+heelGetal = 123
+
+puntGetal *= 2
+
+restWaarde = puntGetal % 3
+
+print(naam)
+print(aanUit)
+print(puntGetal)
+print(karakter)
+print(heelGetal)
+print(restWaarde) 
+
+
+uitkomst = heelGetal / puntGetal
+print(uitkomst)
+
+stukjeTekst = "Hallo"
+stukjeTekst += " mijn naam is " + naam
+print(stukjeTekst)
---
+++ voorbeeld.py
@@ -0,0 +1,15 @@
+#Doel: Voor en achternaam kunnen invoeren. De computer weergeeft mijn volledige naam.
+
+
+
+print("Wat is jouw voornaam?")
+
+naam = input()
+
+
+print("Wat is jou achternaam")
+
+achternaam = input() 
+
+
+print("Jou volledige naam is: " + naam + " " + achternaam)
