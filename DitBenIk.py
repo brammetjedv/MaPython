fef1a6be393e03f487e91b50e265ec16e110c6a7
brammetjedv <bramdevries2003@gmail.com>
2020-09-16 12:08:58 +0200

    PLay with python

--- DitBenIk.py
+++ DitBenIk.py
@@ -15,10 +15,8 @@ import datetime
 tijd = datetime.datetime.now()
 
 antwoord = input("schrijf yes of no: ") 
-
 if antwoord == "yes" or antwoord == "Yes":
     print(tijd.strftime("%A") + " " + tijd.strftime("%d") + " " + tijd.strftime("%B") + " " + tijd.strftime("%X"))
-
 elif antwoord != "yes" or antwoord != "Yes": 
     print("Ok... Dan niet")
 
