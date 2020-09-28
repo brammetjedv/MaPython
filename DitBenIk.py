58cc058e4f7a65a8a651a8a653bdc89a5667de97
brammetjedv <bramdevries2003@gmail.com>
2020-09-28 03:12:33 +0200

    Ditben ik beroepsopdracht

--- DitBenIk.py
+++ DitBenIk.py
@@ -14,12 +14,67 @@ import datetime
 
 tijd = datetime.datetime.now()
 
-antwoord = input("schrijf yes of no: ") 
-if antwoord == "yes" or antwoord == "Yes":
+antwoord1 = input("schrijf yes of no: ") 
+if antwoord1 == "yes" or antwoord1 == "Yes":
     print(tijd.strftime("%A") + " " + tijd.strftime("%d") + " " + tijd.strftime("%B") + " " + tijd.strftime("%X"))
-elif antwoord != "yes" or antwoord != "Yes": 
+elif antwoord1 != "yes" or antwoord1 != "Yes": 
     print("Ok... Dan niet")
 
-print("Bedankt voor het doen van mijn script")
+print("Oke nu wat vragen over mij")
 
-exit
+print("waar woon ik?")
+print("A. Amsterdam")
+print("B. Haarlem")
+print("C. Abcoude")
+
+antGoed = 0
+
+antwoord2 = input("A   B  of  C?")
+if antwoord2 == "A" or antwoord2 == "a":
+    print("Ja goed!")
+    antGoed += 1
+elif antwoord2 == "B" or antwoord2 == "b":
+    print("Nee helaas het was: A. Amsterdam")
+elif antwoord2 == "C" or antwoord2 == "c":
+    print("Nee helaas het was: A. Amsterdam")    
+    
+    
+print("Hoeveel huisdieren heb ik?")
+print("A. 1")
+print("B. 4")
+print("C. 3")
+
+
+antwoord3 = input("A   B  of  C?")
+if antwoord3== "C" or antwoord2 == "c":
+    print("Ja goed!")
+    antGoed += 1
+elif antwoord3 == "B" or antwoord2 == "b":
+    print("Nee helaas het was: C. 3")
+elif antwoord3 == "A" or antwoord2 == "a":
+    print("Nee helaas het was: C. 3")
+    
+    
+    
+print("hoe oud ben ik?")
+print("A. 17")
+print("B. 16")
+print("C. 18")
+
+
+antwoord4 = input("A   B  of  C?")
+if antwoord4 == "A" or antwoord2 == "a":
+    print("Ja goed!")
+    antGoed += 1
+elif antwoord4 == "B" or antwoord2 == "b":
+    print("Nee helaas het was: A. 17")
+elif antwoord4 == "C" or antwoord2 == "c":
+    print("Nee helaas het was: A. 17")
+    
+    
+    
+    
+    
+print("Je had " + str(antGoed) + " antwoorden goed!")
+    
+    
